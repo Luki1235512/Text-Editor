5530eac727a62ce7a2aabeef111b21ce788c98eb
Luki1235512 <Luki1235512@gmail.com>
2025-03-06 18:16:09 +0100

    Basic line viewer

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -11,17 +11,11 @@ pub struct View {
 }
 
 impl View {
-    pub fn render(&self) -> Result<(), Error> {
+    pub fn render_welcome_screen() -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
 
         for current_row in 0..height {
             Terminal::clear_line()?;
-
-            if let Some(line) = self.buffer.lines.get(current_row) {
-                Terminal::print(line)?;
-                Terminal::print("\r\n")?;
-                continue;
-            }
 
             #[allow(clippy::integer_division)]
             if current_row == height / 3 {
@@ -32,6 +26,30 @@ impl View {
             if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
+        }
+        Ok(())
+    }
+
+    pub fn render_buffer(&self) -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
+
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            if let Some(line) = self.buffer.lines.get(current_row) {
+                Terminal::print(line)?;
+                Terminal::print("\r\n")?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn render(&self) -> Result<(), Error> {
+        if self.buffer.is_empty() {
+            Self::render_welcome_screen()?;
+        } else {
+            self.render_buffer()?;
         }
         Ok(())
     }
@@ -49,7 +67,13 @@ impl View {
         welcome_message.truncate(width);
         Terminal::print(&welcome_message)?;
         Ok(())
-}
+    }
+
+    pub fn load(&mut self, file_name: &str) {
+        if let Ok(buffer) = Buffer::load(file_name) {
+            self.buffer = buffer;
+        }
+    }
 
     fn draw_empty_row() -> Result<(), Error> {
         Terminal::print("~")?;

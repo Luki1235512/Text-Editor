68fc58681f43d04d09040b93d484455111e28beb
Luki1235512 <luki1235512@gmail.com>
2025-04-03 21:14:01 +0200

    Add Scrolling

--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,22 +1,23 @@
-use std::io::Error;
+use super::line::Line;
 use std::fs::read_to_string;
+use std::io::Error;
 
 #[derive(Default)]
 pub struct Buffer {
-  pub lines: Vec<String>,
+    pub lines: Vec<Line>,
 }
 
 impl Buffer {
-  pub fn load(file_name: &str) -> Result<Self, Error> {
-    let contents = read_to_string(file_name)?;
-    let mut lines = Vec::new();
-    for value in contents.lines() {
-      lines.push(String::from(value));
+    pub fn load(file_name: &str) -> Result<Self, Error> {
+        let contents = read_to_string(file_name)?;
+        let mut lines = Vec::new();
+        for value in contents.lines() {
+            lines.push(Line::from(value));
+        }
+        Ok(Self { lines })
     }
-    Ok(Self { lines })
-  }
 
-  pub fn is_empty(&self) -> bool {
-    self.lines.is_empty()
-  }
+    pub fn is_empty(&self) -> bool {
+        self.lines.is_empty()
+    }
 }
